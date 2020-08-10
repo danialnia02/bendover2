21ae11fcb65ef2c9f12d4e5732583ba5e3d3a6be
danialnia02 <danial.blackops2@gmail.com>
2020-08-10 19:31:58 +0800

    push

---
+++ frontend/src/components/ExploreContainer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div className="container">
+      <strong>Ready to create an app?</strong>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default ExploreContainer;
